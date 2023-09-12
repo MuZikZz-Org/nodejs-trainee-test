41485ae59611b65d62f9f3279494098630650511
paintparisut <92552125+paintparisut@users.noreply.github.com>
2023-09-12 15:12:03 +0700

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './server.js',  // Replace with your entry file
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')  // Replace with your output directory
-  }
-};
