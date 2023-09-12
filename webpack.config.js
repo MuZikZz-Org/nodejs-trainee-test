02fc31c0a0f4b9d500ed2526155fa1f3d4f84b92
paintparisut <92552125+paintparisut@users.noreply.github.com>
2023-09-12 15:07:18 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',  // Replace with your entry file
+  entry: './server.js',  // Replace with your entry file
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')  // Replace with your output directory
